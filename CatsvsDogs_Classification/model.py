ec5af9d352054c74b71fa313c73ff507f9e660df
unlum212 <muhammetunlu@gmail.com>
2022-10-10 11:07:29 +0300

    commit

---
+++ CatsvsDogs_Classification/model.py
@@ -0,0 +1,67 @@
+import os
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+import matplotlib.pyplot as plt
+import numpy
+from torch.utils.data import Dataset, DataLoader
+import glob
+from PIL import Image
+
+
+class CNN_1(nn.Module):
+    def __init__(self, input_size, n_feature, output_size):
+        super(CNN_1, self).__init__()
+        self.n_feature = n_feature
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=n_feature, kernel_size=5)
+        self.conv2 = nn.Conv2d(n_feature, n_feature, kernel_size=5)
+        self.fc1 = nn.Linear(n_feature * 53 * 53, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x, verbose=False):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, kernel_size=2)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, kernel_size=2)
+        x = x.view(-1, self.n_feature * 53 * 53)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.log_softmax(x, dim=1)
+        return x
+
+
+class CNN_2(nn.Module):
+    def __init__(self, input_size, n_feature, output_size):
+        super(CNN_2, self).__init__()
+        self.n_feature = n_feature
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=5)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=5)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=5)
+        self.conv4 = nn.Conv2d(128, 128, kernel_size=5)
+        self.fc1 = nn.Linear(128 * 10 * 10, 50)
+        self.fc2 = nn.Linear(50, 2)
+
+    def forward(self, x, verbose=False):
+        x = self.conv1(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, kernel_size=2)
+        x = self.conv2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, kernel_size=2)
+        x = self.conv3(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, kernel_size=2)
+        x = self.conv4(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, kernel_size=2)
+        x = x.view(-1, 128 * 10 * 10)
+        x = self.fc1(x)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.log_softmax(x, dim=1)
+        return x
